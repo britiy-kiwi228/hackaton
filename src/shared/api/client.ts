89cac2df4616b1413e919ff12e95bc6ace74c381
britiy-kiwi228 <kostikovmihail078@gmail.com>
2025-12-05 18:01:32 +0300

    подключаем бекенд к фронту

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosError } from 'axios';
 
-const API_BASE_URL = (import.meta as any).env.VITE_API_URL || 'http://localhost:8000';
+const API_BASE_URL = (import.meta as any).env.VITE_API_URL || 'http://localhost:8000/api';
 
 const client: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
